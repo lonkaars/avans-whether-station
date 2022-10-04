1000c8fd861b046a72bb3957dcc04cd6b28ceede
lonkaars <loek@pipeframe.xyz>
2022-10-04 13:39:20 +0200

    return to manual led on/off state management

--- stm32f091/main.c
+++ stm32f091/main.c
@@ -5,12 +5,11 @@
 #include <stm32f0xx_hal.h>
 
 void task_1() {
-	// uint8_t led = 1;
+	uint8_t led = 1;
 
 	while (1) {
-		// HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, led);
-		// led ^= 1;
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, led);
+		led ^= 1;
 
 		vTaskDelay(1000 / portTICK_RATE_MS);
 	}
